1620e2493ccc0249f1acb6bcc6d6fc744233d51e
Lorenzo Boglione <boglione.lorenzo99@gmail.com>
2024-12-15 17:51:23 +0000

    Sensing: Testing COLMAP
    Signed-off-by: Lorenzo Boglione <boglione.lorenzo99@gmail.com>

--- .devcontainer/ros-jammy-cuda/postCreate.sh
+++ .devcontainer/ros-jammy-cuda/postCreate.sh
@@ -1,1 +1,38 @@
 echo "source /opt/ros/jazzy/setup.bash" >> ~root/.bashrc
+
+# Testing COLMAP
+sudo apt-get install \
+    git \
+    cmake \
+    ninja-build \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libeigen3-dev \
+    libflann-dev \
+    libfreeimage-dev \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libgmock-dev \
+    libsqlite3-dev \
+    libglew-dev \
+    qtbase5-dev \
+    libqt5opengl5-dev \
+    libcgal-dev \
+    libceres-dev
+
+# Fixing bug?
+sudo apt-get install gcc-10 g++-10
+export CC=/usr/bin/gcc-10
+export CXX=/usr/bin/g++-10
+export CUDAHOSTCXX=/usr/bin/g++-10
+
+git clone https://github.com/colmap/colmap.git
+cd colmap
+mkdir build
+cd build
+
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j5
